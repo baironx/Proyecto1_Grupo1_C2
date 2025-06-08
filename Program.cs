3aa93a866446e8f355065eed5637252d91bb1f4c
baironx <bairon.gg.alvarado@hotmail.com>
2025-06-07 19:26:56 -0600

    Primer commit

--- Program.cs
+++ Program.cs
@@ -1,1 +1,15 @@
-﻿// Proyecto 1 Sistema de Registro de Entrenamiento Personal.POO
+﻿//Programa principal: App Sistema de Registro de Entrenamiento Personal
+
+using System;
+
+namespace AppSistemaRegistroEntrenamientoPersonal.POO
+{
+    public class program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
+
