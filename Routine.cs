0cbb108243bc1b3432125d0777129009c7eec5c6
Grinch0317 <Chancleta0317@gmail.com>
2025-06-11 22:35:50 -0600

    Update Routine.cs

--- Routine.cs
+++ Routine.cs
@@ -7,6 +7,23 @@ using System.Threading.Tasks;
 namespace Proyecto1_Grupo1_C2.Entities
 {
     public abstract class Routine
+    {    public string Name { get; set; }// nombre de la rutina
+    public int Duration { get; set; } // en minutos
+    public string Intensity { get; set; } // si va ser baja , media o alta 
+    public string Description { get; set; }// descripcion de la rutina
+    public DateTime CreatedDate { get; set; }// fecha de creacion de la rutina
+}
+ Routine()
     {
+        CreatedDate = DateTime.Now;
+    }
+    protected Routine(string name, int duration, string intensity, string description)
+    {
+       this.Name = name;
+        this.Duration = duration;
+       this.Intensity = intensity;
+        this.Description = description;
+        CreatedDate = DateTime.Now;
+    }
     }
 }
