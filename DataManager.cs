d1784b6ad1171cb5d2a398526ce57d2d8715ad38
IsaacR778 <valeriorojas7789@gmail.com>
2025-06-14 22:03:32 -0600

    Update DataManager.cs
    Funcionas de carga y guardado agregadas, falta la de migraci[on de datos, quedo atento a cambios

--- DataManager.cs
+++ DataManager.cs
@@ -12,6 +12,39 @@ namespace Proyecto1_Grupo1_C2.Services
     {
         private static string athletesFilePath = "athletes.txt";
         private static string routinesFilePath = "routines.txt";
+        // esta clase guarda,carga y migra datos
+        public static void SaveData(list<Athlete> athletes, list<Routine>routines)
+        {
+            using (StreamWriter writer = new StreamWriter(athletesFilePath))// guarda los atletas en el txt
+            {
+                foreach(var athlete in athletes)
+                {
+                    writer.WriteLine($"{atlete.id}/{atlete.Name}/{athlete.Age}"); // Escribe los datos y los separa con una /
+                }
+            }
     }
+        public static void LoadData(out List<Athlete>athletes, out List<Routine>routines)//Metodo de carga de txt
+        {
+            athletes=new List<Athlete>();
+            routines=new List<Routine>();
+
+            if (file.Exists(athleteFilePath))//carga los datos desde el athletes.txt si los archivos existen para procurar que no se caiga el programa
+            {
+                foreach (var line in File.ReadAllLines(athletesFilePath))
+                {
+                    var parts =line.Split("/");//se separan los datos con simbolo
+                    if (parts.length >=3)
+                    {
+                        athletesAdd(new Athlete
+                        {
+                            Id = int.Parse(parts[0]), Name = parts[1], Age = int.Parse(parts[2])//Creamos un nuevo atleta y se agrega a la lista existente
+                        });
+                    }
+                }
+            }  
+        }
+    // falta migrar datos
+                                
 }
-Console.WriteLine("Hola perros")
+            
+
