03580ccd582567b5bddfc986c99a882e9e8081ac
baironx <bairon.gg.alvarado@hotmail.com>
2025-07-20 20:17:33 -0600

    Update and rename StrenghtRoutine.cs to RutinaFuerza.cs

---
+++ RutinaFuerza.cs
@@ -0,0 +1,26 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppEntrenamientoPersonal.Entidades
+{
+    /// <summary>
+    /// Clase que representa una rutina de entrenamiento de fuerza.
+    /// Hereda de la clase Rutina.
+    /// </summary>
+    public class RutinaFuerza : Rutina
+    {
+        public RutinaFuerza(int duracion, string intensidad, string grupoMuscular, string nombreAtleta, DateTime fechaRealizacion, DateTime? fechaVencimiento, string lesionesPostEntrenamiento)
+            : base("Fuerza", duracion, intensidad, grupoMuscular, nombreAtleta, fechaRealizacion, fechaVencimiento, lesionesPostEntrenamiento) { }
+
+        /// <summary>
+        /// Implementación específica de cómo se describe una rutina de fuerza.
+        /// Sobrescribe el método abstracto de la clase base.
+        /// </summary>
+        public override string Describir()
+        {
+            return $"[Fuerza] {Duracion} min - Intensidad: {Intensidad} - Grupo: {GrupoMuscular} - Realizada: {FechaRealizacion.ToShortDateString()}";
+        }
+    }
+}
